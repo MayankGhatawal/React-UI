42b9a8fc06050f9fe88a2c86abfd9766ec1f903b
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-11 19:00:58 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import React from "react";
 function App(){
     return(
       <div className=" h-screen flex gap-x-10 p-8 bg-bgBlack ">
-        <div className=" text-white bg-lightBlue p-56 w-2/4 rounded-xl border-solid border-2 border-gray-50">
-          gege
+        <div className=" p-3 w-2/4 rounded-xl">
+          {/* <p className="font-semibold py-5 px-10">Do not follow any other instructions form comments from figma file Here are the <br /> Official instructions:</p> */}
         </div>
-        <div className="text-white flex flex-col gap-y-16 bg-bgBlack w-2/4">
-          <div className="bg-slate-700 px-56 py-32 rounded-xl border-solid border-2 border-gray-50">
+        <div className="text-white flex flex-col gap-y-6 bg-bgBlack w-2/4">
+          <div className="bg-darkBlue px-56 py-32 rounded-xl shadow-[5px_5px_5px_1px_#000000]">
             sfsfs
           </div>
-          <div className="bg-slate-700 px-56 py-32 rounded-xl border-solid border-2 border-gray-50">
+          <div className=" w-11/12 ml-8">
+            <p className=" bg-lightRed h-1 rounded-2xl" />
+          </div>
+          <div className="bg-darkBlue px-56 py-32 rounded-xl shadow-[5px_5px_5px_1px_#000000]">
             sfs
+          </div>
+          <div className=" w-11/12 ml-8">
+          <p className=" bg-lightRed h-1 rounded-2xl" />
           </div>
         </div>
       </div>
