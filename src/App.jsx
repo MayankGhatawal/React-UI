798a3c48c6ccc6f5a0f8faaeec1dbd4a7b5bfce2
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-11 18:28:16 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 function App(){
     return(
-      <>
-        
-      </>
+      <div className=" h-screen flex gap-x-10 p-8 bg-bgBlack ">
+        <div className=" text-white bg-lightBlue p-56 w-2/4 rounded-xl border-solid border-2 border-gray-50">
+          gege
+        </div>
+        <div className="text-white flex flex-col gap-y-16 bg-bgBlack w-2/4">
+          <div className="bg-slate-700 px-56 py-32 rounded-xl border-solid border-2 border-gray-50">
+            sfsfs
+          </div>
+          <div className="bg-slate-700 px-56 py-32 rounded-xl border-solid border-2 border-gray-50">
+            sfs
+          </div>
+        </div>
+      </div>
     );
 }
 
